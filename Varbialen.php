435b7a79ccb23539dc818d69ef9c7b71eb6605b1
Justin <justin-z@live.nl>
2018-09-05 09:25:32 +0200

    PHP varbialen

---
+++ Varbialen.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Justin
+ * Date: 5-9-2018
+ * Time: 9:19
+ */
+$aantal = 14;
+
+
+$deze_jongen = "deze jongen is";
+
+
+echo $deze_jongen.$aantal."jaar oud!";
